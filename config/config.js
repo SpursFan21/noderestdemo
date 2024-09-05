f230ef231aa0ef321b8c02496d401e9a5b946f14
SpursFan21 <duncanfrederic@gmail.com>
2024-09-06 10:35:17 +1200

    up

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@
  
 let env = process.env.NODE_ENV || 'dev'   // 1
 let mongodb_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/NZChat;';
+//let mongodb_url = process.env.MONGODB_URL || 'mongodb+srv://12Batwom34:12Batwom34@nzchat.evbarcr.mongodb.net/nodejsdemo';
  
 console.log('the env is ' + env);
 process.env.NODE_ENV  = env;
